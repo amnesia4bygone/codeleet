5ef79370c795ad232ac755849a022e27d39981b0
Qiao Yong <amnesia.bygone@gmail.com>
2012-12-02 21:54:31 +0800

    subsets

---
+++ SubSets.cpp
@@ -0,0 +1,50 @@
+class Solution {
+    public:
+
+
+        void add_one_element(int x,  vector<vector<int> >  &result)
+        {
+            for (int i=0; i<result.size(); i++)
+            {
+                vector<int > one = result[i];
+
+                one.push_back(x);
+
+                result.push_back(one);
+            }
+        }
+
+        vector<vector<int> > subsets(vector<int> &S) {
+            // Start typing your C/C++ solution below
+            // DO NOT write int main() function
+            vector<vector<int> >  result;
+
+            int len = S.size();
+
+            if (len == 0)
+                return result;
+
+            sort(S.begin(), S.end());
+
+            vector<int > one;
+            result.push_back(one);
+
+            // i means element number of subset
+            for (int i=0; i<S.size(); i++)
+            {
+                int number = result.size();
+                for (int j=0; j<number; j++)
+                {
+                    vector<int > one = result[j];
+
+                    one.push_back(S[i]);
+
+                    result.push_back(one);
+                }
+            }
+
+            return result;
+
+
+        }
+};
