a991c2b6a29732de51b4431d219c5634a9437761
Qiao Yong <amnesia.bygone@gmail.com>
2012-12-09 22:34:06 +0800

    dynamic programing, key point is: i is in end positon of subset solution

--- m/MaxSubarray.cpp
+++ m/MaxSubarray.cpp
@@ -1,75 +1,39 @@
-#include <vector>
-#include "stdio.h"
-#include <algorithm>
-
-using namespace std;
-
 class Solution {
     public:
+
+        vector<int > tmp_val;
+
         int maxSubArray(int A[], int n) {
             // divide and conquer
-
+            //return 1;
+            if (n==0)
+                return 0;
             if (n==1)
                 return A[0];
 
-            int left = maxSubArray(A, n/2  );
-            int right = maxSubArray(A+n/2, (n+1)/2);
+            //return 1;
 
-            int i=(n-1)/2;
-            int j=(n+1)/2;
 
-            int maxi = -9999;
-            int maxj = -9999;
+            int max = -99999;
+            int last_max = -99999;
 
-            vector<int> result;
-            int sum = 0;
-            while(i>=0)
+            for(int i=0; i<n; i++)
             {
-                sum += A[i];
-                result.push_back(sum);
-                i--;
-            }
-            for(int x=0; x<result.size(); x++)
-            {
-                if ( maxi < result[x] )
-                    maxi = result[x];
+                if (A[i] > A[i]+ last_max)
+                {
+                    last_max = A[i];
+                }
+                else
+                {
+                    last_max += A[i];
+                }
+
+                if (max < last_max)
+                    max = last_max;
             }
 
-            sum = 0;
-            result.clear();
-            while(j<=n-1)
-            {
-                sum += A[j];
-                result.push_back(sum);
-                j++;
-            }
-            for(int x=0; x<result.size(); x++)
-            {
-                if ( maxj < result[x] )
-                    maxj = result[x];
-            }  
-            int max  = maxj + maxi;
 
-            result.clear();
-            result.push_back(max);
-            result.push_back(maxj);
-            result.push_back(maxi);
-            result.push_back(left);
-            result.push_back(right);
-            sort(result.begin(), result.end());
-            return result[4];
+            return max;
 
         }
 };
-
-
-int main(void)
-{
-        //[1,2,-1,-2,2,1,-2,1,4,-5,4]
-        //int A[11] = {1,2,-1,-2,2,1,-2,1,4,-5,4,};
-        int A[] = {-9,9,7,-8,-3,2,-9,1,7,-2,-9,7,-9,5,5,-3,9,0};
-        Solution s;
-        int x = s.maxSubArray(A, 18);
-        printf("%d\n", x);
-        return 0;
-}
