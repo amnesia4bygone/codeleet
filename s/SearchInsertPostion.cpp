1767f8b0b094b3dfc0ad5f880bf07b98749b5009
amnesia4bygone <amnesia.bygone@gmail.com>
2012-12-30 17:23:57 +0800

    sort color not pass

--- s/SearchInsertPostion.cpp
+++ s/SearchInsertPostion.cpp
@@ -1,25 +1,47 @@
 class Solution {
-    public:
-        int searchInsert(int A[], int n, int target) {
-            // Start typing your C/C++ solution below
-            // DO NOT write int main() function
-            int * p = A;
-
-            for (int i=0; i<n; i++)
-            {
-                int tmp = *p;
-
-                if (tmp == target)
-                    return i;
-
-                if (tmp > target)
-                    return i;
-
-                p++;
-
-
-
-            }
-            return n;
+public:
+    int searchInsert(int A[], int n, int target) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int begin =0;
+        int end = n-1;
+        
+        if (n==0)
+            return 0;
+        
+        if (n==1)
+        {
+            if (target > A[0])
+                return 1;
+            else
+                return 0;
         }
+        
+        while(begin  < end )
+        {
+            int mid = (begin + end + 1 ) /2;
+            
+            if (target == A[mid])
+                return mid;
+            
+            if (begin +1 == end)
+            {
+                if (target > A[end])
+                    return end+1;
+                else if (target < A[begin] )
+                    return begin;
+                else if (target == A[begin])
+                    return begin;
+                else
+                    return end;
+            }
+            
+            if (target > A[mid])
+                begin  = mid;
+            else
+                end = mid;
+        }
+        return begin;
+        
+    }
 };
--- s/SortColor.cpp
+++ s/SortColor.cpp
@@ -1,66 +1,90 @@
 #include <stdio.h>
 
 class Solution {
-    public:
-        void sortColors(int A[], int n) {
-            // Start typing your C/C++ solution below
-            // DO NOT write int main() function
-            int *p_end = A+n -1;
-            int *p_start = A;
 
-            int *p_0 = A;
-            int *p_2 = A;
-
-
-
-            while (p_end > p_start )
+    
+public:
+    void dump(int A[], int n)
+    {
+	for(int i=0;i<n;i++)
+		printf("%d ", A[i]);
+ 	printf("\n");	
+    }
+    void sortColors(int A[], int n) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        
+        if (n<=1 )
+            return;
+        
+        if (n<=2)
+        {
+            if (A[0]>A[1])
             {
-                while (p_0 - A < n  && *p_0 != 0  )
-                {
-                    p_0++;
-                }
-                    printf("1 %d\n", p_0 - A);
-
-                while(p_2 - A < n && *p_2 != 2) 
-                {
-                    p_2 ++;
-                }
-                    printf("2 %d\n", p_2 - A);
-
-                int tmp;
-
-                if (p_0 - A != n && p_end > p_start)
-                {
-                    printf("11 %d\n", p_0 - A);
-                    tmp = *p_start;
-                    *p_start = *p_0;
-                    *p_0 = tmp;
-                    p_start++;
-
-                }
-
-                if (p_2 - A != n && p_end > p_start )
-                {
-                    printf("22 %d\n", p_2 - A);
-                    tmp = *p_end;
-                    *p_end = *p_2;
-                    *p_2 = tmp;
-                    p_end--;
-                }
-
-                if (p_0 == A+n && p_2 == A+n)
-                    break;
+                int tmp = A[0];
+                A[0] = A[1];
+                A[1] = tmp;
             }
-
+            return;
         }
+        
+        int tail_offset = n-1;
+        int zero_offset = 0;
+        int one_offset = n-1;
+        
+        while( zero_offset<=one_offset && zero_offset <= tail_offset && one_offset <= tail_offset )
+        {
+            while(A[zero_offset] == 0 && zero_offset<n)
+            {
+                zero_offset++;
+            }
+            
+            while(A[one_offset] == 1 && one_offset>=0)
+            {
+                one_offset--;
+            }
+            
+            while(A[tail_offset] == 2 && tail_offset>=0)
+            {
+                tail_offset--;
+            }
+            
+            if (zero_offset>one_offset && zero_offset>tail_offset && one_offset>tail_offset)
+                break;
+            dump(A,n);
+	    printf("%d %d %d\n", zero_offset, one_offset, tail_offset); 
+            if (A[zero_offset]==2)
+            {
+                int tmp = A[zero_offset];
+                A[zero_offset] = A[tail_offset];
+                A[tail_offset] = tmp;
+            }
+            else if (A[one_offset] == 2)
+            {
+                int tmp = A[one_offset];
+                A[one_offset] = A[tail_offset];
+                A[tail_offset] = tmp;                
+            }
+            else
+            {
+                int tmp = A[zero_offset];
+                A[zero_offset] = A[one_offset];
+                A[one_offset] = tmp;
+            }
+            dump(A,n);
+        }
+        
+        return;
+    }
 };
 
 int main(void)
 {
-        int A[2];
-        A[0] = 2;
-        A[1] = 0;
+        int A[3];
+        A[0] = 0;
+        A[1] = 2;
+        A[2] = 1;
         Solution s;
-        s.sortColors(A,2);
+        s.sortColors(A,3);
         return 0;
 }
