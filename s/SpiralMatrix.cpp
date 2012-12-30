d7e74f2ea8a1055fadfed85667a20a91cd942922
amnesia4bygone <amnesia.bygone@gmail.com>
2012-12-30 19:51:40 +0800

    SM

---
+++ s/SpiralMatrix.cpp
@@ -0,0 +1,99 @@
+#include <vector>
+
+#include "stdio.h"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int> > &matrix) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        vector<int > result;
+        
+        int row_num = matrix.size() ;
+        if (row_num == 0)
+            return result;
+                        
+        int col_num = matrix[0].size();
+       
+        
+        int loop_offset = 0;
+        int direction = 0; // 0, to right; 1, down, 2, to left, 3, up
+        int step = col_num;
+        int cur_row = 0;
+        int cur_col = 0;
+        
+        while(step > 0)
+        {
+
+            if( direction == 0)
+            {
+                for(int i=loop_offset; i<step; i++)
+                {
+                    result.push_back(matrix[loop_offset][i]);
+			printf("1 %d\n", matrix[loop_offset][i]);
+                    cur_col++;
+                }
+                step = row_num - 2*loop_offset -1;
+                
+                direction = 1;
+            }
+            else if (direction == 1)
+            {
+                for(int i=1+loop_offset; i< row_num- loop_offset; i++)
+                {
+                    result.push_back(matrix[i][col_num - loop_offset -1]);
+			printf("2 %d\n", matrix[i][col_num - loop_offset-1]);
+                    cur_row++;
+                }
+                step = col_num - 2*loop_offset -1;
+                direction =2 ;
+            }
+            else if (direction == 2)
+            {
+  		// bug here
+                for(int i= col_num - 2 - loop_offset; i>=loop_offset; i--)
+                {
+                    result.push_back(matrix[row_num - loop_offset-1][i]);
+			printf("3 %d\n", matrix[row_num - loop_offset-1][i]);
+                    cur_col--;
+                }
+                step = row_num - 2*loop_offset -2;
+                direction = 3;
+            }
+            else
+            {
+                for(int i=row_num - loop_offset; i>loop_offset; i-- )
+                {
+                    result.push_back(matrix[i][loop_offset]);
+			printf("4 %d\n", matrix[i][loop_offset]);
+                    cur_row--;
+                }                
+                direction = 0;
+                loop_offset++;
+                step = col_num -2*loop_offset;
+                
+            }
+        
+        }
+        
+        return result;
+    }
+};
+
+int main(void)
+{
+	vector<int> a1;
+	vector<int> a2;
+	a1.push_back(1);
+	a1.push_back(2);
+	a2.push_back(4);
+	a2.push_back(3);
+	vector<vector<int> > matrix;
+	matrix.push_back(a1);
+	matrix.push_back(a2);
+
+	Solution s;
+	s.spiralOrder(matrix);
+}
