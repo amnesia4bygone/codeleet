4733945ee0d79f1856a46426f622b5209ce2ed16
amnesia4bygone <amnesia.bygone@gmail.com>
2013-01-03 22:32:00 +0800

    DS

---
+++ DistinctSubSequence.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+
+    int len_s;
+    int len_t;
+    
+
+    int numDistinct(string S, string T) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        len_s = S.length();
+        len_t = T.length();
+        
+        if (len_s == 0 || len_t == 0 || len_s < len_t)
+        {
+            return 0;
+        }
+        
+        
+        vector<vector<int> > store = vector<vector<int> >(T.length()+1, vector<int>(S.length()+1, 0));
+
+        store[0][0] = 0;
+        store[1][0] = 0;
+        store[0][1] = 0;
+        if ( S[0] == T[0] )
+            store[1][1] = 1;
+        else
+            store[1][1] = 0;
+        
+        /*
+        for(int i=1; i<=len_s; i++)
+        {
+            if (S[i] == T[0])
+            {
+                store[1][i] = store[1][i-1] +1;
+            }
+            else
+            {
+                store[1][i] = store[1][i-1];
+            }
+        }
+        
+        
+        for(int j=2; j<=len_t; j++)
+        {
+            
+        }*/
+        
+        for(int j=1; j<=len_t; j++ )
+        {
+            for(int i=2; i<=len_s; i++)
+            {   
+                int keep = 0;
+                if (S[i-1] == T[j-1])
+                    keep = store[j-1][i-1];
+                int drop = store[j][i-1];
+                
+                store[j][i] = keep + drop;
+            }
+        }
+        
+        return store[ len_t][len_s];
+        
+    }
+};
--- UniquePathII.cpp
+++ UniquePathII.cpp
@@ -1,106 +1,71 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
+        // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        if (obstacleGrid.size() == 1)
-        {
-            if (obstacleGrid[0][0] != 1)
-                return 1;
-            else
-                return 0;
-        }
-        int m = obstacleGrid.size() +1;
-        int n = obstacleGrid[0].size() +1;
-                
+        int m = obstacleGrid.size();
+        if (m<1)
+            return 0;
         
-        vector<vector<int> > route;
+        int n = obstacleGrid[0].size();
+        if (n<1)
+            return 0;
         
-        vector<int> one_line ;
-        for(int i=0; i<m;i++)
-        {
-            route.push_back(one_line);
-            route[i].push_back(1);
-        }
-        for (int i=0; i<n; i++)
-            route[0][i] = 1;
         
-          
-        for(int i=1; i<=m ; i++)
+        int * route = (int *) malloc( n*m*sizeof(int) );
+        if (obstacleGrid[0][0] == 0)
+            route[0] = 1;
+        else
+            return 0;
+        
+        // deal with line0, except j=0
+        int flag = 0;
+        for(int j=1; j<n; j++)
         {
-            for(int j=1; j<=n; j++)
+            if(flag == 1 || obstacleGrid[0][j] == 1)
             {
-                if(i==1 && j==1)
+                route[j] = 0;
+                //obstacleGrid[0][j] = 1;   
+                flag = 1;
+            }
+            else
+                route[j] = 1;
+        }
+        
+        // deal with col0, except i=0
+        flag = 0;
+        for(int i=1; i<m; i++)
+        {
+            if(flag == 1 || obstacleGrid[i][0] == 1)
+            {
+                route[i*n] = 0;
+                //obstacleGrid[i][0] = 1;   
+                flag = 1;
+            }
+            else
+                route[i*n] = 1;            
+        }
+        
+        for(int i=1; i<m; i++)
+        {
+            for (int j=1; j<n; j++)
+            {
+                int top = route[ (i-1)*n + j];
+                int left = route[i*n + j-1];
+                
+                if (obstacleGrid[i][j] == 1)
                 {
-                    route[1][1] = 1;
-                    continue;
+                    route[i*n + j] = 0;
                 }
-                
-                int left; 
-                int top;
-                
-                if ( obstacleGrid[i-1][j] ==1 )
-                    top = 0;
                 else
-                    top = route[i-1][j];
-                    
-                if (j==0|| obstacleGrid[i][j-1] ==1 )
-                    left = 0;
-                else
-                    left = route[i][j-1];
-                
-                route[i][j] = top + left;
+                {
+                    route[i*n + j] = top + left;
+                }
             }
         }
-        return route[m-1][n-1];
-        
-/*
-        for(int i=1; i<m+n; i++)
-        {
-             int x = 0;
-             int y = i;
-
-             
-             while(x < m)
-             {
-                 if (y>=n)
-                 {
-                     y = n-1;
-                     x = i-y;
-                 }
-                 if (x>=m)
-                 {
-                     x =m-1;
-                     y = i - x;
-                 }
-             
-             
-                 int left;
-                 if (obstacleGrid[x][y-1] == 0 )
-                    left = route[x][y-1];
-                 else
-                    left = 0;
-                    
-                 int top;
-                 if (obstacleGrid[x-1][y] == 0) 
-                    top = route[x-1][y];
-                 else
-                    top = 0;
-                 
-                 
-                 
-                 if (x==0)
-                    route[x].push_back(left);
-                 else if (y==0)
-                    route[x].push_back(top);
-                 else
-                    route[x].push_back( top + left  ) ;
-                
-                 x++;
-             }
-        }*/
-        
-        
-        
+        int ret = route[ (m-1)*n  + n-1];
+        free(route ); route = NULL;
+        return ret;
         
     }
 };
