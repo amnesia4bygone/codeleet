4fc9b811d2384829c0f54bb18a92d3c867dea3fa
amnesia4bygone <amnesia.bygone@gmail.com>
2013-01-04 23:18:20 +0800

    DS

--- DistinctSubSequence.cpp
+++ DistinctSubSequence.cpp
@@ -6,8 +6,6 @@ public:
     
 
     int numDistinct(string S, string T) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
         len_s = S.length();
         len_t = T.length();
         
